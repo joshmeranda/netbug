2c911f3cec7eaa0b745516f4f47139a0d953edcb
joshmeranda <joshmeranda@gmail.com>
2021-02-26 22:08:15 -0500

    Move capture responsibility into client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -19,69 +19,7 @@ fn main() {
         }
     };
 
-    let client = Client::from_config(client_cfg);
+    let mut client = Client::from_config(client_cfg);
 
-    let capture_flag = Arc::new(Mutex::new(true));
-
-    // find the list of valid devices on which to start a packet capture
-    let devices = Device::list().unwrap();
-    let devices: Vec<Device> = devices
-        .into_iter()
-        .filter(|device| {
-            client_cfg.interfaces.contains(&device.name)
-        })
-        .collect();
-
-    // keeps track that at least one interface has started a packet capture
-    let mut capture_started = false;
-
-    for device in devices {
-        let flag = Arc::clone(&capture_flag);
-        let device_name = String::from(device.name.clone());
-        let capture_result = device.open();
-
-        match capture_result {
-            Ok(mut capture) => {
-
-                let mut save_file = capture
-                    .savefile(format!("{}.pcap", &device_name)).unwrap();
-
-                let builder = Builder::new().name(device_name.clone());
-
-                // todo: check that the thread was started successfully
-                // todo: add timestamp to end pf pcap name
-                let handle = builder.spawn(move || while *flag.lock().unwrap() {
-                    let packet = capture.next();
-
-                    if packet.is_ok() {
-                        save_file.write(&packet.unwrap());
-                    }
-                });
-
-                match handle {
-                    Ok(_) => println!("Started capture for device '{}'", device_name),
-                    Err(err) => println!("Err: {}", err.to_string())
-                }
-
-                capture_started = true;;
-            }
-            Err(err) => {
-                eprintln!(
-                    "Unable to create a capture for device '{}'\n{}",
-                    device_name,
-                    err.to_string()
-                )
-            }
-        }
-    }
-
-    if capture_started {
-        // client_cfg.run_scripts();
-
-        // give the captures some time to read all packets from buffer
-        // todo: make configurable
-        std::thread::sleep(std::time::Duration::new(10, 0));
-    }
-
-    *capture_flag.lock().unwrap() = false;
+    client.start_capture();
 }
--- src/lib/client.rs
+++ src/lib/client.rs
@@ -1,31 +1,46 @@
 use std::process::{Child, Command};
 use std::path::{PathBuf, Path};
 use std::io;
+use std::default::Default;
 
 use crate::config::client::ClientConfig;
 use crate::config::defaults;
+use pcap::Device;
+use std::thread::{Builder, JoinHandle};
+use std::sync::Arc;
 
+#[derive(Default)]
 pub struct Client {
     script_dir: PathBuf,
 
     allow_concurrent: bool,
 
+    devices: Vec<Device>,
 
+    capture_handles: Vec<JoinHandle<()>>
 }
 
 impl Client {
     pub fn new() -> Client {
-        Client {
-            script_dir: defaults::default_script_dir(),
-            allow_concurrent: false
-        }
+        Client::default()
     }
 
     /// Construct a client from the client configuration
     pub fn from_config(cfg: ClientConfig) -> Client {
+        // find the list of valid devices on which to start a packet capture
+        let devices = Device::list().unwrap();
+        let devices: Vec<Device> = devices
+            .into_iter()
+            .filter(|device| {
+                cfg.interfaces.contains(&device.name)
+            })
+            .collect();
+
         Client {
             script_dir: cfg.script_dir,
-            allow_concurrent: cfg.allow_concurrent
+            allow_concurrent: cfg.allow_concurrent,
+            devices,
+            .. Client::default()
         }
     }
 
@@ -66,7 +81,50 @@ impl Client {
         Ok(())
     }
 
-    pub fn start_capture(&self) -> Result<(), io::Error> {
+    pub fn start_capture(&mut self) -> Result<(), ()> {
+        // todo: fix Error types
+        for device in &self.devices {
+            let device_name = String::from(device.name.clone());
+            let capture_result = device.clone().open();
+
+            match capture_result {
+                Ok(mut capture) => {
+
+                    let mut save_file = capture
+                        .savefile(format!("{}.pcap", &device_name)).unwrap();
+
+                    let builder = Builder::new().name(device_name.clone());
+
+                    // todo: check that the thread was started successfully
+                    // todo: add timestamp to end pf pcap name
+                    let handle = builder
+                        .spawn(move || loop {
+                            let packet = capture.next();
+
+                            if packet.is_ok() {
+                                save_file.write(&packet.unwrap());
+                            }
+                        });
+
+                    match handle {
+                        Ok(handle) => {
+                            self.capture_handles.push(handle);
+                            println!("Started capture for device '{}'", device_name)
+                        },
+                        Err(err) => println!("Err: {}", err.to_string())
+                    }
+                }
+                Err(err) => {
+                    eprintln!(
+                        "Unable to create a capture for device '{}'\n{}",
+                        device_name,
+                        err.to_string()
+                    )
+                }
+            }
+        }
+
+        Ok(())
     }
 
     pub fn transfer_pcaps(&self) {
